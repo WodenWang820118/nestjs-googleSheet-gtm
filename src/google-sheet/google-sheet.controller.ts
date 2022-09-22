597729067daf4f0fb5b926c170c8f479fca271f1
Guan-Xin <alex0972112871@gmail.com>
2022-09-22 11:55:43 +0800

    update: re-organize google sheet api calls and functions

--- src/google-sheet/google-sheet.controller.ts
+++ src/google-sheet/google-sheet.controller.ts
@@ -7,30 +7,13 @@ export class GoogleSheetController {
 
   @Get('/specs')
   async readGoogleSheet(): Promise<any> {
-    // TODO: locate for the range in the google sheet
-    // should be a specified sheet name with title keyword and translate it into a range
-    const range = 'Sheet1!A:A';
-    const rawSpecs = [];
-    const specs = [];
-
     const auth = await this.googleSheetService.authorize();
-    const content = await this.googleSheetService.listSpecs(auth, range);
-
-    for (let row = 0; row < content.length; row++) {
-      for (let col = 0; col < content[row].length; col++) {
-        if (content[row][col].startsWith('window')) {
-          rawSpecs.push(content[row][col]);
-        }
-      }
-    }
-
-    for (let i = 0; i < rawSpecs.length; i++) {
-      const result = this.googleSheetService.getSpecs(rawSpecs[i]);
-      if (this.googleSheetService.isValidJson(result)) {
-        specs.push(JSON.parse(result));
-      }
-    }
-
-    return JSON.stringify(specs);
+    const range = await this.googleSheetService.getSpecsRange();
+    console.log(`The specs range is ${range}; please specify it in .env`);
+    const content = await this.googleSheetService.getSheet(auth, range);
+    // checkpoint for debugging, rawSpecs not freindly sometimes from the sheet
+    const rawSpecs = this.googleSheetService.getRawSpecs(content);
+    const specs = this.googleSheetService.getJsonSpecs(rawSpecs);
+    return specs;
   }
 }
