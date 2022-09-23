25451105341a33323cdb8b29a9c64e53c245ea9c
Guan-Xin <alex0972112871@gmail.com>
2022-09-23 11:23:51 +0800

    func: init gtm api to access accounts, containers, and workspace

--- src/gtm/gtm.controller.ts
+++ src/gtm/gtm.controller.ts
@@ -1,11 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { GtmService } from './gtm.service';
-
 @Controller('gtm')
 export class GtmController {
   constructor(private readonly gtmService: GtmService) {}
 
   @Get('/accounts')
   async listAccounts() {
+    const auth = await this.gtmService.authorize();
+    const accounts = await this.gtmService.listAccounts(auth);
+    return accounts;
+  }
+
+  @Get('/accounts/:accountPath')
+  async getAccountByPath(@Param('accountPath') accountPath: string) {
+    const auth = await this.gtmService.authorize();
+    const path = `accounts/${accountPath}`;
+    const accounts = await this.gtmService.getAccountByPath(auth, path);
+    return accounts;
+  }
+
+  @Get('/accounts/:accountPath/containers')
+  async listContainers(@Param('accountPath') accountPath: string) {
+    const auth = await this.gtmService.authorize();
+    const path = `accounts/${accountPath}`;
+    const containers = await this.gtmService.getContainers(auth, path);
+    return containers;
+  }
+
+  @Get('/accounts/:accountPath/containers/:containerPath')
+  async getContainerByPath(
+    @Param('accountPath') accountPath: string,
+    @Param('containerPath') containerPath: string,
+  ) {
+    const auth = await this.gtmService.authorize();
+    const path = `accounts/${accountPath}/containers/${containerPath}`;
+    const container = await this.gtmService.getContainerByPath(auth, path);
+    return container;
+  }
+
+  @Get('/accounts/:accountPath/containers/:containerPath/workspaces')
+  async listWorkspaces(
+    @Param('accountPath') accountPath: string,
+    @Param('containerPath') containerPath: string,
+  ) {
+    const auth = await this.gtmService.authorize();
+    const path = `accounts/${accountPath}/containers/${containerPath}`;
+    const workspaces = await this.gtmService.getWorkspaces(auth, path);
+    return workspaces;
+  }
+
+  @Get(
+    '/accounts/:accountPath/containers/:containerPath/workspaces/:workspacePath',
+  )
+  async getWorkspaceByName(
+    @Param('accountPath') accountPath: string,
+    @Param('containerPath') containerPath: string,
+    @Param('workspacePath') workspacePath: string,
+  ) {
+    const auth = await this.gtmService.authorize();
+    const path = `accounts/${accountPath}/containers/${containerPath}/workspaces/${workspacePath}`;
+    const workspace = await this.gtmService.getWorkspaceByPath(auth, path);
+    return workspace;
   }
 }
--- src/gtm/gtm.service.ts
+++ src/gtm/gtm.service.ts
@@ -1,7 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { google } from 'googleapis';
 
+const SCOPES = [
+  'https://www.googleapis.com/auth/tagmanager.manage.accounts',
+  'https://www.googleapis.com/auth/tagmanager.edit.containers',
+  'https://www.googleapis.com/auth/tagmanager.delete.containers',
+  'https://www.googleapis.com/auth/tagmanager.edit.containerversions',
+  'https://www.googleapis.com/auth/tagmanager.manage.users',
+  'https://www.googleapis.com/auth/tagmanager.publish',
+];
 @Injectable()
 export class GtmService {
+  async authorize() {
+    const auth = new google.auth.GoogleAuth({
+      scopes: SCOPES,
+      keyFile: 'gtm_client_secrets.json',
+    });
+    const authClient = await auth.getClient();
+    google.options({ auth: authClient }); // bind it to future calls
+    return authClient;
+  }
 
+  async listAccounts(auth: any) {
+    const gtm = google.tagmanager({ version: 'v2', auth });
+    const res = await gtm.accounts.list();
+    return res.data;
+  }
+
+  async getAccountByPath(auth: any, path: string) {
+    const gtm = google.tagmanager({ version: 'v2', auth });
+    const res = await gtm.accounts.get({ path: path });
+    return res.data;
+  }
+
+  async getContainers(auth: any, path: string) {
+    const gtm = google.tagmanager({ version: 'v2', auth });
+    const res = await gtm.accounts.containers.list({ parent: path });
+    return res.data;
+  }
+
+  async getContainerByPath(auth: any, path: string) {
+    const gtm = google.tagmanager({ version: 'v2', auth });
+    const res = await gtm.accounts.containers.get({
+      path: path,
+    });
+    return res.data;
+  }
+
+  async getWorkspaces(auth: any, path: string) {
+    const gtm = google.tagmanager({ version: 'v2', auth });
+    const res = await gtm.accounts.containers.workspaces.list({
+      parent: path,
+    });
+    return res.data;
+  }
+
+  async getWorkspaceByPath(auth: any, path: string) {
+    const gtm = google.tagmanager({ version: 'v2', auth });
+    const res = await gtm.accounts.containers.workspaces.get({
+      path: path,
+    });
+    return res.data;
+  }
 }
